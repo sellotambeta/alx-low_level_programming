6bc4906436e29c3967db74a27ebab6c24978d10a
sellotambeta <sellotambet@gmail.com>
2023-03-20 07:35:38 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
-#include<stdio>
+#include<stdio.h>
 
 /**
  * main - proper grammer
  *
- * Return : 0
+ * Return : 0  when succesfull
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
