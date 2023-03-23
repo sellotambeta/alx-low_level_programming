eb83879566ab32ef29c2f19bf6a2ce3c6c9aac3c
sellotambeta <sellotambet@gmail.com>
2023-03-23 04:58:29 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i = '0');
+		else if
+			{
+				putchar(i + '0');
+				putchar(',');
+				putchar(' ');
+			}
+	}
+	return (0)
+}
