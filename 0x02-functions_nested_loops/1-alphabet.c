8bcf30321b32ed6a3ed5024864081922871f7141
sellotambeta <sellotambet@gmail.com>
2023-03-25 07:26:50 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print alphabet - function that return small letters
+ * Return: return 0
+ */
+
+
+void print_alphabet(void);
+{
+	chat i;
+
+	for (i = 'a' ; i <= 'z' ; i++
+		_putchar(i);
+	_putchar('\n');
+}
