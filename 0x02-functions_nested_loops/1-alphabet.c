a44e2badb30ac8d1f9d6dd314e45fef319427956
sellotambeta <sellotambet@gmail.com>
2023-03-25 07:36:26 -0700

    task2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  */
 
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	chat i;
+	char i;
 
-	for (i = 'a' ; i <= 'z' ; i++
+	for (i = 'a' ; i <= 'z' ; i++)
 		_putchar(i);
 	_putchar('\n');
 }
