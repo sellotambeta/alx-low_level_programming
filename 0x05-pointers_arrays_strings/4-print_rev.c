424ed7c328c59ff022d0f2871954df41d6f90de2
sellotambeta <sellotambet@gmail.com>
2023-03-28 05:04:50 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,20 @@
 #include "main.h"
 
 /**
- * print_rev - 
- * @:
+ * print_rev - print string in reverse
+ * @s: function parameter
  * Return: return 0
  */
 
- void print_rev(char *s)
+void print_rev(char *s)
+
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = count ; i >= 0 ; i--)
+	       _putchar(s[i]);
+	_putchar('\n');
+}
