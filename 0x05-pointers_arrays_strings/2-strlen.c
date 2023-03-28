cdaa7183ac95905faf5f771d31c64bd78a37fd29
sellotambeta <sellotambet@gmail.com>
2023-03-28 03:35:13 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - strings
+ * Return: return 0
+ * @s: String
+ */ 
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
